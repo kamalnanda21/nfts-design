9d6e6672194176bbd59a0a56b0db4c458d0faf5a
kamalnanda21 <kamalnanda01@gmail.com>
2024-05-22 17:51:47 +0530

    tried for slider fix

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,34 +1,21 @@
 // slick slider
 
-$(document).ready(function() {
-$('.slick-slider').slick({
-    infinite: true,
-    speed: 500,
-    dots: false,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    arrows: false,
-    pauseOnHover: false,
-    adaptiveHeight: true,
-    responsive: [
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: true
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-    ]
-});
-});
+// $('.slider').slick({
+//     slidesToShow: 2,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+// });
+
+// $(document).ready(function() {
+//     $('.slick-slider').slick({
+//         infinite: true,
+//         speed: 500,
+//         dots: false,
+//         slidesToShow: 2,
+//         slidesToScroll: 2,
+//         autoplay: true,
+//         autoplaySpeed: 3000,
+//         arrows: false,
+//     });
+// });

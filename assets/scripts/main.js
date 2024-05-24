2e4af0fc0dc9c164c0821fcf2b5573adbfe9a5b0
kamalnanda21 <kamalnanda01@gmail.com>
2024-05-24 09:32:23 +0530

    fixed slider

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,21 +1,12 @@
 // slick slider
 
-// $('.slider').slick({
-//     slidesToShow: 2,
-//     slidesToScroll: 1,
-//     autoplay: true,
-//     autoplaySpeed: 2000,
-// });
-
-// $(document).ready(function() {
-//     $('.slick-slider').slick({
-//         infinite: true,
-//         speed: 500,
-//         dots: false,
-//         slidesToShow: 2,
-//         slidesToScroll: 2,
-//         autoplay: true,
-//         autoplaySpeed: 3000,
-//         arrows: false,
-//     });
-// });
+$('.slider').slick({
+    infinite: true,
+    speed: 500,
+    dots: false,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+});
